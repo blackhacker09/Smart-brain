b7b95e62d47c237820baba5b47af40dc9b4eda4f
Suraj Verma <56511978+blackhacker09@users.noreply.github.com>
2023-05-11 15:33:58 +0530

    added backendurl

--- src/Component/Signin/Signin.js
+++ src/Component/Signin/Signin.js
@@ -17,7 +17,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://mybackend-h4js.onrender.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
